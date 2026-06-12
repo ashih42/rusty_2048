36fdb7678b174563d87f5c1901f4c85bf64b901b
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-06-12 08:57:17 -0500

    Print the top-level error message for user in stdout.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use rusty_2048::{app::App, app_settings::AppSettings, my_error::MyError};
 
 fn main() {
     if let Err(err) = App::run() {
-        eprintln!("Error: {}", err);
+        println!("Error: {}", err);
 
         if matches!(err, MyError::InvalidCommandLineArgument { .. }) {
-            eprintln!("\n{}", AppSettings::get_usage());
+            println!("\n{}", AppSettings::get_usage());
         }
     }
 }
